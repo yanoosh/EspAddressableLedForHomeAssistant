a8c20475936dab78f02df91c3505a21a4e0afe5f
Janusz Jablonski <januszjablonski.pl@gmail.com>
2020-01-15 01:26:14 +0100

    Improved cylon bounce.

--- EspAddressableLedForHomeAssistant/effect/CylonBounce.cpp
+++ EspAddressableLedForHomeAssistant/effect/CylonBounce.cpp
@@ -1,16 +1,17 @@
 #ifndef _CYLON_BOUNCE_CPP
 #define _CYLON_BOUNCE_CPP
 
+#include <Arduino.h>
 #include "../rgbw.h"
 #include "EffectProcessor.cpp"
 
 class CylonBounce : public EffectProcessor {
  public:
   CylonBounce(Adafruit_NeoPixel *strip, RGBW color) : EffectProcessor(strip) {
-    this->color = strip->Color(color.red, color.green, color.blue);
-    this->colorPal = strip->Color(color.red / 10, color.green / 10, color.blue / 10);
+    this->color = color;
     this->effectSize = strip->numPixels() / 5;
-    this->maxStep = strip->numPixels() - this->effectSize - 2;
+    this->maxStep = strip->numPixels() - this->effectSize;
+    this->multiplier = PI / (this->effectSize + 1);
   }
 
   const char *getName() override {
@@ -22,11 +23,23 @@ class CylonBounce : public EffectProcessor {
       this->sleepStep--;
       return;
     }
+    Serial.print(this->step);
+    Serial.println(".");
 
     this->strip->clear();
-    this->strip->setPixelColor(this->step, this->colorPal);
-    this->strip->fill(this->color, this->step + 1, this->effectSize);
-    this->strip->setPixelColor(this->step + this->effectSize + 1, this->colorPal);
+    for (uint8_t i = 0; i < this->effectSize; i++) {
+      float sinMultiplier = sin(this->multiplier * (float)(i + 1.0));
+      Serial.println(this->step + i);
+      Serial.println(this->color.red * sinMultiplier);
+      Serial.println("next");
+      this->strip->setPixelColor(
+        this->step + i,
+        this->color.red * sinMultiplier,
+        this->color.green * sinMultiplier,
+        this->color.blue * sinMultiplier,
+        this->color.white * sinMultiplier
+      );
+    }
     this->strip->show();
 
     this->step += this->direction;
@@ -34,8 +47,7 @@ class CylonBounce : public EffectProcessor {
       this->step = this->maxStep;
       this->direction = -1;
       this->sleepStep = 5;
-    }
-    if (this->step < 0) {
+    } else if (this->step < 0) {
       this->step = 0;
       this->direction = 1;
       this->sleepStep = 5;
@@ -47,13 +59,13 @@ class CylonBounce : public EffectProcessor {
   }
 
  private:
-  uint16_t color;
-  uint16_t colorPal;
-  uint16_t maxStep;
+  RGBW color;
   uint8_t effectSize;
-  int16_t step = 0;
   uint8_t sleepStep = 0;
-  int8_t direction = 1;
+  int16_t step = 0;
+  int16_t maxStep;
+  int16_t direction = 1;
+  float multiplier;
 };
 
 #endif
